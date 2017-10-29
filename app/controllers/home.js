6d4e6f9aa6a7e76614d4b05d3eef840f9e8f58e8
Hillary Spratt <spratth@gmail.com>
2017-10-29 18:19:41 -0500

    Add support for .gifv and gfycat files.

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -11,16 +11,37 @@ module.exports = (app) => {
 };
 
 router.get('/', (req, res, next) => {
-	Article
-    .find({})
+    Article.find({})
     .then(function(articles) {
-		res.status(200).render('index', {
-			title: 'Cute Animals Forever',
-			articles: articles,
-			subreddits: subreddits
-		});
+        console.log(`first article is:`, articles[0], `\nhas type of:`, typeof(articles[0]));
+        articles.forEach(function(article, i) {
+            article.tagImg = true;
+            article.tagVid = false;
+            let img = article.externalLink;
+            // gfycat sites: https://[[giant]].(bodyOfLink - [[https://]]).[[mp4]]
+            // imgur sites: (bodyOfLink - [[.gifv]] + .[[mp4]])
+            if (img.includes(`.gifv`) && img.includes(`https://i.imgur`)) {
+                article.externalLink = img.replace(`.gifv`, `.mp4`);
+                article.tagImg = false;
+                article.tagVid = true;
+            }
+            // convert this: https://gfycat.com/ElaborateAmusingHorseshoecrab
+            // convert to:   https://giant.gfycat.com/ElaborateAmusingHorseshoecrab
+            if (img.includes(`gfycat`)) {
+                console.log(`${i} image needs converting:`, img);
+                article.externalLink = img.replace(`gfycat.`, `giant.gfycat.`) + `.mp4`;
+                console.log(`${i} has been converted:`, article.externalLink);
+                article.tagImg = false;
+                article.tagVid = true;
+            }
+        });
+        res.status(200).render('index', {
+            title: 'Cute Animals Forever',
+            articles: articles,
+            subreddits: subreddits
+        });
     })
     .catch(function(err) {
-		res.json(err);
+        res.json(err);
     });
 });
